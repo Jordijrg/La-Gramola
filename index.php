1536ce520587f7a35637b3501de8c3b0c1c30b37
Jordi <jrodriguez4@cendrassos.net>
2023-09-14 19:45:22 +0200

    Top

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Granola.css">
+    <title>La Gramola</title>
+</head>
+<body>
+    <nav>
+        <form action="tu_script_de_busqueda.php" method="get">
+            <input type="text" id="busqueda" name="q" placeholder="Ingrese su búsqueda aquí" />
+            <input type="submit" value="Buscar" />
+        </form>
+        <a>Titol</a>
+        <div>Hola</div>
+    </nav>
+</body>
+</html>
