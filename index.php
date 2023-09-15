da0acd3e4b0931759565ff76b40b8c22761d6451
Jordi <jrodriguez4@cendrassos.net>
2023-09-15 19:42:57 +0200

    boff

--- index.php
+++ index.php
@@ -7,13 +7,35 @@
     <title>La Gramola</title>
 </head>
 <body>
-    <nav>
-        <form action="tu_script_de_busqueda.php" method="get">
-            <input type="text" id="busqueda" name="q" placeholder="Ingrese su búsqueda aquí" />
-            <input type="submit" value="Buscar" />
-        </form>
-        <a>Titol</a>
-        <div>Hola</div>
-    </nav>
+    <div class="container">
+        <div class="grid-container">
+        <nav class="nav">
+            <form action="tu_script_de_busqueda.php" method="get">
+                <input type="text" id="busqueda" name="q" placeholder="Ingrese su búsqueda aquí" />
+                <input type="submit" value="Buscar" />
+            </form>
+            <a>Titol</a>
+            <div class="img">Hola</div>
+        </nav>
+        <div class="playlist">
+            <input class="Pcançons" type="button" value="CANÇONS">
+            <p>LES MEVES PLAYLIST</p>
+            <input class="Pcançons" type="button" value="NOM PLAYLIST">
+            <input class="Pcançons" type="button" value="NOM PLAYLIST">
+            <input class="Pcançons" type="button" value="NOM PLAYLIST">
+        </div>
+        <div class="songs">
+            <input class="cançons" type="button" value="TITOL">
+            <input class="cançons" type="button" value="TITOL">
+            <input class="cançons" type="button" value="TITOL">
+            <input class="cançons" type="button" value="TITOL">
+            <input class="cançons" type="button" value="TITOL">
+            <input class="cançons" type="button" value="TITOL">
+        </div>
+        <div class="barra">
+
+        </div>
+    </div>
+    </div>
 </body>
 </html>
